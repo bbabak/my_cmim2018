e84ade1b609667ff1a8283deef2887d788c6d47f
Babak <35435431+bbabak@users.noreply.github.com>
2018-02-07 11:26:48 +0200

    growth population using Euler forward

---
+++ lecture-practice/odeFE.m
@@ -0,0 +1,12 @@
+function [t,y] = odeFE(fun,tspan,y0)
+t=0:tspan(1):tspan(2);
+
+n=length(t);
+
+y=zeros(length(y0),length(t));
+
+y(:,0)=y0(:);
+
+for i=2:n
+    y(:,i)=y(:,i-1)+dt+fun(T(i-1),y(:,i-1))
+end
