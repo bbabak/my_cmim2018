70c435fbc5365ba3d877729fb713a749ddc86497
Babak <35435431+bbabak@users.noreply.github.com>
2018-02-08 09:47:51 +0200

    Solution to damped system-frequency analysis

--- task03/damped_forced_ssdof.m
+++ task03/damped_forced_ssdof.m
@@ -12,118 +12,17 @@ Dx = diff(x, t);
 
 ode = diff(x,t,2)  + zeta*omega*Dx + omega^2*x - 1/m*f*sin(OMEGA*t);
 
-solution_free = dsolve(ode == 0,[Dx(0) == 0, x(0) == A]);
+solution_forced = dsolve(ode == 0,[Dx(0) == 0, x(0) == A]);
 
-disp(solution_free);
- 
-disp(solution_free);
-% Solve with initial conditions
-% $$\left\{ \begin{array}{c}m\ddot{x}+kx=0\\x\left(0\right)=A,\,\dot{x}\left(0\right)=0\end{array}\right.$$
-%
-% syms A real
-% solution_free_rewritten_ic = dsolve(ode == 0, [Dx(0) == 0, x(0) == A] );
-% solution_free_ic = subs(solution_free_rewritten_ic);
-% disp(solution_free_rewritten_ic);
+disp(solution_forced);
+
 % display equation variables
-symvar(solution_free)
-% solution_free_rewrite = simplify(subs(solution_free, sqrt(k/m), omega)); 
-% disp(solution_free_rewrite)
-% Plot solution
-%
-% figure
+symvar(solution_forced)
 
-% figure('Units','inches','Position',[0 0 8 4],'PaperPositionMode','auto');
+figure('Units','inches','Position',[0 0 8 4],'PaperPositionMode','auto');
 
-% fplot(subs(solution_free_rewritten, [A,OMEGA,c,f,k,m,zeta], [0,01,1,4,2,5,0.5]), [0, 10])
-fplot(subs(solution_free, [A,OMEGA,f,m,omega,zeta], [0.05,6,8,10,2,0.5]), [0, 10])
-% fo=2;
-% zeta1=0;
-% n=1;
-% for i=0:0.05:10
-%     A1(n)=fo/sqrt((1-i^2).^2+(2*zeta1*i).^2);
-%     n=n+1;
-%     disp(i)
-% end
-% 
-% zeta2=0.125;
-% n=1;
-% for i=0:0.05:10
-%     A2(n)=fo/sqrt((1-i^2).^2+(2*zeta2*i).^2);
-%     n=n+1;
-% end
-% 
-% zeta3=0.250;
-% n=1;
-% for i=0:0.05:10
-%     A3(n)=fo/sqrt((1-i^2).^2+(2*zeta3*i).^2);
-%     n=n+1;
-% end
-% 
-% zeta4=0.375;
-% n=1;
-% for i=0:0.05:10
-%     A4(n)=fo/sqrt((1-i^2).^2+(2*zeta4*i).^2);
-%     n=n+1;
-% end
-% 
-% zeta5=0.500;
-% n=1;
-% for i=0:0.05:10
-%     A5(n)=fo/sqrt((1-i^2).^2+(2*zeta5*i).^2);
-%     n=n+1;
-% end
-% 
-% zeta6=0.625;
-% n=1;
-% for i=0:0.05:10
-%     A6(n)=fo/sqrt((1-i^2).^2+(2*zeta6*i).^2);
-%     n=n+1;
-% end
-% 
-% zeta7=0.750;
-% n=1;
-% for i=0:0.05:10
-%     A7(n)=fo/sqrt((1-i^2).^2+(2*zeta7*i).^2);
-%     n=n+1;
-% end
-% 
-% zeta8=0.875;
-% n=1;
-% for i=0:0.05:10
-%     A8(n)=fo/sqrt((1-i^2).^2+(2*zeta8*i).^2);
-%     n=n+1;
-% end
-% 
-% zeta9=1;
-% n=1;
-% for i=0:0.05:10
-%     A9(n)=fo/sqrt((1-i^2).^2+(2*zeta9*i).^2);
-%     n=n+1;
-% end
-% zeta10=1.125;
-% n=1;
-% for i=0:0.05:10
-%     A10(n)=fo/sqrt((1-i^2).^2+(2*zeta10*i).^2);
-%     n=n+1;
-% end
-% syms t
-% zeta=0:0.125:1.125;
-% M=f/sqrt(((1-(OMEGA*t/omega)^2).^2+(2*zeta(1)*(OMEGA*t/omega).^2)));
-% fplot(subs(M, [OMEGA,f,omega], [3,1,2]), [0, 5])
-% hold on
-% M=f/sqrt(((1-(OMEGA*t/omega)^2).^2+(2*zeta(2)*(OMEGA*t/omega).^2)));
-% fplot(subs(M, [OMEGA,f,omega], [3,1,2]), [0, 5])
-% hold on
-% M=f/sqrt(((1-(OMEGA*t/omega)^2).^2+(2*zeta(3)*(OMEGA*t/omega).^2)));
-% fplot(subs(M, [OMEGA,f,omega], [3,1,2]), [0, 5])
-% hold on
-% M=f/sqrt(((1-(OMEGA*t/omega)^2).^2+(2*zeta(4)*(OMEGA*t/omega).^2)));
-% fplot(subs(M, [OMEGA,f,omega], [3,1,2]), [0, 5])
-% hold on
-% M=f/sqrt(((1-(OMEGA*t/omega)^2).^2+(2*zeta(3)*(OMEGA*t/omega).^2)));
-% fplot(subs(M, [OMEGA,f,omega], [3,1,2]), [0, 5])
+fplot(subs(solution_forced, [A,OMEGA,f,m,omega,zeta], [0,6,8,10,2,0.5]), [0, 10])
 
-syms t
 i=1;
 for zeta=0:0.125:1.125
     i=i+1;
@@ -132,33 +31,6 @@ end
 fplot(subs(M, [OMEGA,f,omega], [3,2,3]), [0, 5],'LineWidth', 1.0)
 hold on
 
-
-
-% fplot(subs(solution_free_rewritten, [A,OMEGA,c,f,k,m,zeta], [0,5,1,4,2,5,1.9]), [0, 10])
-% M=sqrt(omega^2-OMEGA^2)^2+(c*OMEGA)^2;
-% r=OMEGA/omega;
-% axis([0 10 -10 10]) 
-% set(gca,'Units','normalized','YTick',-10:1:10,'XTick',0:10/10:10,'Position',[.15 .2 .75 .7],'FontUnits','points','FontWeight','normal','FontSize',9,'FontName','Times')
-% figure('Units','inches','Position',[0 0 6 4],'PaperPositionMode','auto');
-% plot(0:.05:10,A1,'b','LineWidth', 1.5)
-% hold on
-% plot(0:.05:10,A2,'c','LineWidth', 1.5)
-% hold on
-% plot(0:.05:10,A3,'g','LineWidth', 1.5)
-% hold on
-% plot(0:.05:10,A4,'k','LineWidth', 1.5)
-% hold on
-% plot(0:.05:10,A5,'r','LineWidth', 1.5)
-% hold on
-% plot(0:.05:10,A6,'m','LineWidth', 1.5)
-% hold on
-% plot(0:.05:10,A7,'y','LineWidth', 1.5)
-% hold on
-% plot(0:.05:10,A8,'c--','LineWidth', 1.5)
-% hold on
-% plot(0:.05:10,A9,'g--','LineWidth', 1.5)
-% hold on
-% plot(0:.05:10,A10,'b--','LineWidth', 1.5)
 grid on
 xlim([0,5]);
 ylim([0,7.5]);
